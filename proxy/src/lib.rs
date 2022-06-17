72728ee8da3044dcc41541aa65f7613cf0d7ddc2
Cuong Tran <tranquoccuong@outlook.com.vn>
2022-06-17 18:01:44 +0700

    Withdraw NEAR from proxy

--- proxy/src/lib.rs
+++ proxy/src/lib.rs
@@ -12,12 +12,15 @@ use near_sdk::collections::LookupMap;
 use near_sdk::json_types::U128;
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{
-    env, near_bindgen, serde_json, AccountId, Balance, BorshStorageKey, PanicOnDefault, Promise, ext_contract, PromiseOrValue, PromiseResult,
+    env, ext_contract, near_bindgen, serde_json, AccountId, Balance, BorshStorageKey,
+    PanicOnDefault, Promise, PromiseOrValue, PromiseResult,
 };
 use utils::WRAP_NEAR_ACCOUNT;
 
 use crate::errors::*;
-use crate::utils::{BRIDGE_CONTRACT, GAS_FOR_WNEAR, GAS_FOR_RESOLVE_WNEAR, GAS_FOR_DEPOSIT, GAS_FOR_RESOLVE_DEPOSIT};
+use crate::utils::{
+    BRIDGE_CONTRACT, GAS_FOR_DEPOSIT, GAS_FOR_RESOLVE_DEPOSIT, GAS_FOR_RESOLVE_WNEAR, GAS_FOR_WNEAR,
+};
 use crate::w_near::ext_wnear;
 
 /// Message parameters to receive via token function call.
@@ -25,7 +28,6 @@ use crate::w_near::ext_wnear;
 #[serde(crate = "near_sdk::serde")]
 #[serde(untagged)]
 enum DappRequest {
-    WrapNear {},
     DepositRefFinance {},
     SwapRefFinance {},
     WithdrawRefFinace {},
@@ -45,25 +47,36 @@ pub struct Proxy {
 #[ext_contract(ext_self)]
 pub trait ProxyContract {
     fn callback_wnear(&mut self, account_id: AccountId, amount: U128);
-    fn callback_withdraw(&mut self, account_id: AccountId, token_id: AccountId, amount: U128);
+    fn callback_withdraw(
+        &mut self,
+        account_id: AccountId,
+        token_id: AccountId,
+        amount: U128,
+        unwrap: bool,
+    );
 }
 
 #[ext_contract(ext_ft)]
 pub trait FtContract {
-    fn ft_transfer_call(&mut self, receiver_id: AccountId, amount: U128, memo: Option<String>, msg: String);
+    fn ft_transfer_call(
+        &mut self,
+        receiver_id: AccountId,
+        amount: U128,
+        memo: Option<String>,
+        msg: String,
+    );
 }
 
 #[ext_contract(ext_bridge)]
 pub trait BridgeContract {
-    fn deposit(&mut self, incognito_address: String,
-    );
+    fn deposit(&mut self, incognito_address: String);
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
 
 pub struct Deposit {
-    incognito_address: String
+    incognito_address: String,
 }
 
 #[near_bindgen]
@@ -86,14 +99,15 @@ impl Proxy {
 
         // ! Storage deposit first to all registed contracts.
 
-        ext_wnear::near_deposit(account_id.clone(), amount, GAS_FOR_WNEAR).
-        then(ext_self::callback_wnear(
-            account_id.clone(),
-            U128(amount),
-            env::current_account_id().clone(),
-            0,
-            GAS_FOR_RESOLVE_WNEAR,
-        )).into()
+        ext_wnear::near_deposit(account_id.clone(), amount, GAS_FOR_WNEAR)
+            .then(ext_self::callback_wnear(
+                account_id.clone(),
+                U128(amount),
+                env::current_account_id().clone(),
+                0,
+                GAS_FOR_RESOLVE_WNEAR,
+            ))
+            .into()
     }
 
     pub fn call_dapp(&mut self, msg: String) {
@@ -102,19 +116,25 @@ impl Proxy {
 
         let message = serde_json::from_str::<DappRequest>(&msg).expect(ERR28_WRONG_MSG_FORMAT);
         match message {
-            DappRequest::WrapNear {} => {}
             DappRequest::DepositRefFinance {} => {}
             DappRequest::SwapRefFinance {} => {}
             DappRequest::WithdrawRefFinace {} => {}
         }
     }
 
-    pub fn withdraw(&mut self, token_id: String, amount: u128, account_id: AccountId, incognito_address: String) -> Promise {
+    pub fn withdraw(
+        &mut self,
+        token_id: String,
+        amount: u128,
+        account_id: AccountId,
+        incognito_address: String,
+    ) -> Promise {
         let sender_id = env::predecessor_account_id();
         assert_eq!(sender_id.to_string(), BRIDGE_CONTRACT);
 
         let mut withdraw_token = WRAP_NEAR_ACCOUNT.to_string();
-        if token_id != "" { // not withdraw NEAR
+        if token_id != "" {
+            // not withdraw NEAR
             withdraw_token = token_id.clone();
         }
         let withdraw_token_id: AccountId = withdraw_token.clone().try_into().unwrap();
@@ -128,7 +148,8 @@ impl Proxy {
 
         self.internal_withdraw_token(&account_id, &withdraw_token_id, withdraw_amount);
 
-        if token_id != "" { // not withdraw NEAR
+        if token_id != "" {
+            // not withdraw NEAR
             let obj = Deposit {
                 incognito_address: incognito_address.clone(),
             };
@@ -141,21 +162,40 @@ impl Proxy {
                 withdraw_token_id.clone(),
                 1,
                 GAS_FOR_DEPOSIT,
-            ).then(ext_self::callback_withdraw(
+            )
+            .then(ext_self::callback_withdraw(
                 account_id.clone(),
                 withdraw_token_id.clone(),
                 U128(withdraw_amount),
-                env::current_account_id().clone(),
+                false,
+                env::current_account_id(),
                 0,
                 GAS_FOR_RESOLVE_DEPOSIT,
-            )).into()
+            ))
+            .into()
         } else {
-            ext_bridge::deposit(
-                incognito_address, 
+            ext_wnear::near_withdraw(
+                U128(withdraw_amount - 1),
+                WRAP_NEAR_ACCOUNT.to_string().try_into().unwrap(),
+                1,
+                GAS_FOR_WNEAR,
+            )
+            .then(ext_bridge::deposit(
+                incognito_address,
                 bridge_id,
                 withdraw_amount,
                 GAS_FOR_DEPOSIT,
-            ).into()
+            ))
+            .then(ext_self::callback_withdraw(
+                account_id.clone(),
+                withdraw_token_id.clone(),
+                U128(withdraw_amount),
+                true,
+                env::current_account_id(),
+                0,
+                GAS_FOR_RESOLVE_DEPOSIT,
+            ))
+            .into()
         }
     }
 
@@ -176,23 +216,42 @@ impl Proxy {
         }
     }
 
-    pub fn callback_withdraw(&mut self, account_id: AccountId, token_id: AccountId, amount: U128) {
-        assert_eq!(env::promise_results_count(), 1, "This is a callback method");
+    pub fn callback_withdraw(
+        &mut self,
+        account_id: AccountId,
+        token_id: AccountId,
+        amount: U128,
+        unwrap: bool,
+    ) {
+        let mut num_promise: u64 = 1;
+        if unwrap {
+            num_promise = 2;
+        }
+        assert_eq!(
+            env::promise_results_count(),
+            num_promise,
+            "This is a callback method"
+        );
 
-        // handle the result from the second cross contract call this method is a callback for
-       match env::promise_result(0) {
+        // handle the result from the last cross contract call this method is a callback for
+        match env::promise_result(num_promise - 1) {
             PromiseResult::NotReady => unreachable!(),
             PromiseResult::Failed => {
                 self.internal_deposit_token(&account_id, &token_id, amount.into());
+                return;
             }
-            PromiseResult::Successful(result) => {}
+            PromiseResult::Successful(_result) => {}
         };
     }
 }
 
 /// Internal methods implementation.
 impl Proxy {
-    pub(crate) fn internal_get_balance_token(&self, account_id: &AccountId, token_id: &AccountId) -> Balance {
+    pub(crate) fn internal_get_balance_token(
+        &self,
+        account_id: &AccountId,
+        token_id: &AccountId,
+    ) -> Balance {
         let account = self.internal_unwrap_account(account_id);
         account.get_balance_token(token_id)
     }
--- proxy/src/w_near.rs
+++ proxy/src/w_near.rs
@@ -1,7 +1,8 @@
-use near_sdk::{ext_contract};
+use near_sdk::ext_contract;
+use near_sdk::json_types::U128;
 
 #[ext_contract(ext_wnear)]
 pub(crate) trait WNearContract {
     fn near_deposit(&mut self);
+    fn near_withdraw(&mut self, amount: U128);
 }
-
